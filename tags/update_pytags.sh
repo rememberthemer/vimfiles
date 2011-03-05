600373405e20add58830ae96c690124cda24555d
bensonad <adrian.m.benson@gmail.com>
2011-03-06 08:18:14 +1300

    fixed python tags script

--- tags/update_pytags.sh
+++ tags/update_pytags.sh
@@ -22,7 +22,6 @@ if [ `lsb_release -is` == "Arch" ]; then
     find /usr/lib/python${py2} -type f -name '*.py' |
         ctags -R --fields=+i --extra=+q --filter=yes > ~/.vim/tags/python${py2}.tags
     py3=`python -V 2>&1 | cut -d' ' -f2`
-    py3=${py3%.*}
     echo $py3
     find /usr/lib/python${py3} -type f -name '*.py' |
         ctags -R --fields=+i --extra=+q --filter=yes > ~/.vim/tags/python${py3}.tags
