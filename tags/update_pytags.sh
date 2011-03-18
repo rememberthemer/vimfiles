db58bdbdfa00bb6fe180081f6d3b307fd51fe3cc
bensonad <adrian.m.benson@gmail.com>
2011-03-19 12:59:23 +1300

    edit pytags script

--- tags/update_pytags.sh
+++ tags/update_pytags.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 if [ `lsb_release -is` == "Arch" ]; then
-    py2=`python2 -V 2>&1 | cut -d' ' -f2`
-    py2=${py2%.*}
-    echo $py2
-    find /usr/lib/python${py2} -type f -name '*.py' |
-        ctags -R --fields=+i --extra=+q --filter=yes > ~/.vim/tags/python${py2}.tags
-    py3=`python -V 2>&1 | cut -d' ' -f2`
-    echo $py3
-    find /usr/lib/python${py3} -type f -name '*.py' |
-        ctags -R --fields=+i --extra=+q --filter=yes > ~/.vim/tags/python${py3}.tags
+    py2=`python2 -V 2>&1 | cut -d' ' -f2 | cut -d'.' -f1,2`
+    py2="python${py2}"
+    echo =================================================
+    echo Tags for ${py2}
+    echo =================================================
+    ctags -R --fields=+i --extra=+q --sort=yes --languages=Python \
+        -o ~/.vim/tags/${py2}.tags /usr/lib/${py2}
+    
+    py3=`python -V 2>&1 | cut -d' ' -f2 | cut -d'.' -f1,2`
+    py3="python${py3}"
+    echo =================================================
+    echo Tags for${py3}
+    echo =================================================
+    ctags -R --fields=+i --extra=+q --sort=yes --languages=Python \
+        -o ~/.vim/tags/${py3}.tags /usr/lib/${py3}
 fi
