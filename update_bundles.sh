bbddf497eaa6bf26a8dab316b07171d5f8fb9dc3
bensonad <adrian.m.benson@gmail.com>
2011-04-07 07:07:36 +1200

    make bundle update commits automatic

--- update_bundles.sh
+++ update_bundles.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ `git status -s | wc -l` -ne 0 ]; then
+if [ $(git status -s | wc -l) -ne 0 ]; then
     echo "git repo is not clean, commit changes before proceeding"
     exit 1
 fi
 
 git submodule foreach "git pull origin master"
 
-if [ -n "`git status -s | grep ' M bundle/'`" ]; then
-    git commit -a
+if [ -n "$(git status -s | grep ' M bundle/')" ]; then
+    git commit -a -m "bundle update"
 fi
 
